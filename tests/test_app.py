bb4f111aa1ef9c65496d6cf6c9d61d4eca3b818d
anisul-islam-prog <anisul1@g.bracu.ac.bd>
2026-07-10 01:45:40 +0600

    fix: shotti final fix

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,9 @@ from app.models import Task
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
+    app.config['WTF_CSRF_ENABLED'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     with app.test_client() as client:
         with app.app_context():
